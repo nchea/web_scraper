48db4fd0204e6176a2d1f973fec065a583925b3b
nchea <nchea.sf@gmail.com>
2022-07-13 21:11:35 -0700

    this is the initial web scraper

---
+++ web_scraper.rb
@@ -0,0 +1,9 @@
+puts "Hello, Ruby!"
+
+greeting = "Good morning"
+puts "#{greeting}. What a splendid day it is."
+puts "#{greeting}" << ", David."
+
+color = ["green", "yellow", "red", "black"]
+puts "The first color is: #{color.first}"
+puts "The last color is: #{color.last}"
